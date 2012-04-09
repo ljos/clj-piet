2afd3e5b20a353b9212c247a6047a0d400b84ab4
Bjarte Johansen <bjo013@uib.no>
2012-04-09 20:21:14 +0200

    changed version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-piet "1.0.0-SNAPSHOT"
+(defproject clj-piet "0.0.1"
   :description "Interpreter for piet written in cljoure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :jvm-opts ["-Djava.awt.headless=true"]
