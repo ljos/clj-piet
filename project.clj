dedfbed131d0fb91d735760df1e862460c171377
Bjarte Johansen <bjo013@uib.no>
2012-04-17 16:30:32 +0200

    Upgraded Clojure to 1.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-piet "0.0.1"
   :description "Interpreter for piet written in cljoure"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :jvm-opts ["-Djava.awt.headless=true"]
   :main clj-piet.core)
